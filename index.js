d917584cc8ac5c3d4144fc140285adba14ca0825
Mansi Mishra <mansimishra510@gmail.com>
2021-09-10 20:02:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const searchForm = document.querySelector("form");
 const searchResultDiv = document.querySelector(".search-result");
 const container = document.querySelector(".container");
 let searchQuery = "";
-
-
 const APP_ID = "ea6b460c";
 const APP_key = "e6822c6e487c788c767eec6943ec12c4";
 // console.log(container)
@@ -14,8 +12,6 @@ searchForm.addEventListener("submit", (e) => {
 });
 
 async function fetchAPI() {
-  
-//   I am adding this baseURL link from edamam recipe site 
   const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_key}&from=0&to=20`;
   const response = await fetch(baseURL);
   const data = await response.json();
@@ -29,7 +25,6 @@ function generateHTML(results) {
   let generatedHTML = "";
   results.map((result) => {
     generatedHTML += `<div class="item">
-    
     <img src="${result.recipe.image}" alt="">
     <div class="flex-container">
       <h1 class="title">${result.recipe.label}</h1>
@@ -41,7 +36,15 @@ function generateHTML(results) {
             : "No Data Found"
         }</p>
       </div>
+    `
+  //   `<div class="item">
+  //   <img src="https://images2.minutemediacdn.com/image/upload/c_crop,h_1126,w_2000,x_0,y_181/f_auto,q_auto,w_1100/v1554932288/shape/mentalfloss/12531-istock-637790866.jpg" alt="">
+  //   <div class="flex-container">
+  //     <h1 class="title">This is a recipe item</h1>
+  //     <a class="view-btn" href="#">View Recipe</a>
+  //   </div>
+  //   <p class="item-data">Calories: 120</p>
+  // </div>`
   });
   searchResultDiv.innerHTML = generatedHTML;
 }
-
